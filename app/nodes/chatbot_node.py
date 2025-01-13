9ee8ee42ff238204cb7ad189e127147286d6ce36
Nonetss <amorenolopezbarajas@gmail.com>
2025-01-13 14:13:35 +0100

    Hacemos que la IA use el embeding

--- app/nodes/chatbot_node.py
+++ app/nodes/chatbot_node.py
@@ -5,12 +5,27 @@ from template.crear_respuesta import CrearRespuesta
 
 def chatbot(state: State):
     question = state["question"]
+    about = state["about"]
+    doc = state["doc"]
     crear_prompt = CrearPrompt()
-    prompt = crear_prompt.generar_prompt(
-        nombre_prompt="requestDoc", prompt_humano=question
-    )
     response = CrearRespuesta()
-    result = response.generar_respuesta(prompt)
-    print(result)
-    state["question"] = ""
-    return state
+    if about == "otro":
+        prompt = crear_prompt.generar_prompt(
+            nombre_prompt="requestDoc", prompt_humano=question
+        )
+        result = response.generar_respuesta(prompt)
+        print(result)
+        state["question"] = ""
+        return state
+    else:
+        prompt = crear_prompt.generar_prompt(
+            nombre_prompt="requestDoc",
+            prompt_humano=question,
+            prompt_document=doc,
+        )
+        result = response.generar_respuesta(prompt)
+        print(result)
+        state["question"] = ""
+        state["doc"] = [""]
+        state["about"] = ""
+        return state
