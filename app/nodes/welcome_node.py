c418aa8d2f053c809acc6468ed18ee5f739f52c6
Nonetss <amorenolopezbarajas@gmail.com>
2025-01-13 12:30:12 +0100

    Nodo de bienvenida completado

--- app/nodes/welcome_node.py
+++ app/nodes/welcome_node.py
@@ -2,7 +2,11 @@ from app.state import State
 
 
 def welcome(state: State):
-    state["response"] = "¡Hola! Soy NoneBot, tu asistente personal. ¿Cómo te llamas?"
-    print(state["response"])
-    state["user_name"] = input("Tu nombre: ")
+    # Solo pedir el nombre si no está definido
+    user_name = input(
+        "¡Hola! Soy NoneBot, tu asistente personal. ¿Cómo te llamas?\nTu nombre: "
+    )
+    state["user_name"] = user_name
+    print(f"¡Encantado de conocerte, {state['user_name']}! ¿En qué puedo ayudarte hoy?")
+
     return state
