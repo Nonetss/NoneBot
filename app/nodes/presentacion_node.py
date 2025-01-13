e8b1ab4a77d88396408fe1736e2c29bd09c69ca6
Nonetss <amorenolopezbarajas@gmail.com>
2025-01-13 12:29:40 +0100

    Eliminamos nodo inutil

--- app/nodes/presentacion_node.py
+++
@@ -1,9 +0,0 @@
-from app.state import State
-
-
-def presentacion(state: State):
-    user_name = state.get("user_name", "desconocido")
-    state["question"] = input(
-        f"¡Encantado de conocerte, {user_name}! ¿En qué puedo ayudarte hoy?"
-    )
-    return state
