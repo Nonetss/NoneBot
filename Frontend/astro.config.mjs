3f5aa0b73403a7bd959582e6a4652fce556aa098
Nonetss <amorenolopezbarajas@gmail.com>
2025-02-16 18:27:09 +0100

    Introducimos la configuracion para que pueda usar .env

--- Frontend/astro.config.mjs
+++ Frontend/astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  vite: {
+    envPrefix: "VITE_", // Asegura que solo se expongan variables con el prefijo "VITE_"
+  },
 });
